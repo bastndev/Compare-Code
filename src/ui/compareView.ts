93378c90405fc2ac9e8c37b5dbda16dd715a97e6
bastndev <gohit24@gmail.com>
2025-09-17 14:37:55 -0500

    🌺  [Update] Refactor styling by replacing global.css with SCSS files, updating package.json scripts, and enhancing webview styles

--- src/ui/compareView.ts
+++ src/ui/compareView.ts
@@ -52,10 +52,10 @@ export function isViewOpen(): boolean {
 	return comparePanel !== undefined && comparePanel.visible;
 }
 
-// Generates HTML content for the webview
+// Generates HTML content for the webview MARK:HTML / CSS
 function getWebviewContent(context: vscode.ExtensionContext, webview: vscode.Webview): string {
-	// Get the CSS file URI
-	const cssPath = vscode.Uri.file(path.join(context.extensionPath, 'src', 'ui', 'webview', 'styles', 'global.css'));
+	// Get the CSS file URI (compiled from SCSS)
+	const cssPath = vscode.Uri.file(path.join(context.extensionPath, 'src', 'ui', 'webview', 'styles', 'main.css'));
 	const cssUri = webview.asWebviewUri(cssPath);
 
 	// Read the HTML template
