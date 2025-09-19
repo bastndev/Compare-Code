e29c4d838c0832e016c4966ae7d3bcb4d81752fb
bastndev <gohit24@gmail.com>
2025-09-18 19:48:19 -0500

    🏗️ [Add] Introduce new SVG icons and update webview styles for enhanced UI functionality

--- src/ui/webview/view/webviewIcons.ts
+++ src/ui/webview/view/webviewIcons.ts
@@ -8,9 +8,25 @@ export function getWebviewIcons(context: vscode.ExtensionContext, webview: vscod
     const playPath = vscode.Uri.file(path.join(context.extensionPath, 'assets', 'icons', 'play.svg'));
     const playUri = webview.asWebviewUri(playPath);
 
+    const on1Path = vscode.Uri.file(path.join(context.extensionPath, 'assets', 'icons', 'on1.svg'));
+    const on1Uri = webview.asWebviewUri(on1Path);
+
+    const on2Path = vscode.Uri.file(path.join(context.extensionPath, 'assets', 'icons', 'on2.svg'));
+    const on2Uri = webview.asWebviewUri(on2Path);
+
+    const on3Path = vscode.Uri.file(path.join(context.extensionPath, 'assets', 'icons', 'on3.svg'));
+    const on3Uri = webview.asWebviewUri(on3Path);
+
+    const on4Path = vscode.Uri.file(path.join(context.extensionPath, 'assets', 'icons', 'on4.svg'));
+    const on4Uri = webview.asWebviewUri(on4Path);
+
     return {
         warning: warningUri.toString(),
         play: playUri.toString(),
+        on1: on1Uri.toString(),
+        on2: on2Uri.toString(),
+        on3: on3Uri.toString(),
+        on4: on4Uri.toString(),
     };
 }
 
@@ -19,6 +35,10 @@ export function replaceIconsInHtml(html: string, icons: ReturnType<typeof getWeb
     
     processedHtml = processedHtml.replace('{{WARNING_ICON}}', icons.warning);
     processedHtml = processedHtml.replace('{{PLAY_ICON}}', icons.play);
+    processedHtml = processedHtml.replace('{{ON1_ICON}}', icons.on1);
+    processedHtml = processedHtml.replace('{{ON2_ICON}}', icons.on2);
+    processedHtml = processedHtml.replace('{{ON3_ICON}}', icons.on3);
+    processedHtml = processedHtml.replace('{{ON4_ICON}}', icons.on4);
     
     return processedHtml;
 }
